10500266cadd4fc15d0146453fb378c647102042
sun <743685264@qq.com>
2023-08-16 13:01:03 +0800

    医院详情菜单与子路由

--- src/pages/hospital/index.vue
+++ src/pages/hospital/index.vue
@@ -1,13 +1,78 @@
 <template>
-    <div>
-
+    <div class="hospital">
+        <!-- 左侧导航区域 -->
+        <div class="menu">
+            <div class="top">
+                <el-icon><HomeFilled /></el-icon>
+                <span> / 医院详情信息</span>
+            </div>
+            <el-menu default-active="/hospital/register" class="el-menu-vertical-demo" router>
+                <el-menu-item index="/hospital/register">
+                    <template #title>
+                        <el-icon>
+                            <Clock />
+                        </el-icon>
+                        <span>预约挂号</span>
+                    </template>
+                </el-menu-item>
+                <el-menu-item index="/hospital/detail">
+                    <el-icon><icon-menu /></el-icon>
+                    <span>医院详情</span>
+                </el-menu-item>
+                <el-menu-item index="/hospital/notice">
+                    <el-icon>
+                        <document />
+                    </el-icon>
+                    <span>预约须知</span>
+                </el-menu-item>
+                <el-menu-item index="/hospital/close">
+                    <el-icon>
+                        <Warning />
+                    </el-icon>
+                    <span>停诊信息</span>
+                </el-menu-item>
+                <el-menu-item index="/hospital/search">
+                    <el-icon>
+                        <Search />
+                    </el-icon>
+                    <span>查询/取消</span>
+                </el-menu-item>
+            </el-menu>
+        </div>
+        <!-- 右侧内容展示区域：路由组件展示位置 -->
+        <div class="content">
+            <!-- 子组件展示结构地方 -->
+            <RouterView></RouterView>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+// 左侧菜单需要用到的图标
+import {
+    Document, Menu as IconMenu, Search, Clock, Warning, HomeFilled
+} from '@element-plus/icons-vue'
+
 
 </script>
 
 <style scoped lang="scss">
+.hospital {
+    display: flex;
 
+    .menu {
+        flex: 2;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .top {
+        color: #7f7f7f;
+
+    }
+    .content {
+        flex: 8;
+    }
+}
 </style>
---
+++ src/pages/hospital/register/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        挂号组件
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
